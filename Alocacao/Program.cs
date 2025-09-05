37dc69056f3503ab0dbb71523003a7d6d7201bec
2880482411021 <2880482411021@LAB05DSK22>
2025-09-05 08:29:34 -0300

    Adicionar arquivos de projeto.

---
+++ Alocacao/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection.PortableExecutable;
+using System.Runtime.InteropServices;
+
+namespace Alocacao
+{
+    internal class Program
+    {
+
+        class Carro{
+            public string Cor;
+            public string Nome;
+
+        };
+
+        static void Main(string[] args)
+        {
+            
+            Carro carro1 = new Carro();
+            Carro carro2 = new Carro();
+
+            carro1.Cor = "Azul";
+            carro1.Nome = "Gol";
+
+            carro2.Cor = "Azul";
+            carro2.Nome = "Gol";
+
+            bool teste = carro1 == carro2;
+
+
+        }
+    }
+}
